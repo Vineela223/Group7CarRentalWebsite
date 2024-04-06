e1fdd71a8143075fc075bbf7ff0b03eee194beef
Vineela223 <150950991+Vineela223@users.noreply.github.com>
2024-04-05 21:31:32 -0500

    Add files via upload

---
+++ Home.php
@@ -0,0 +1,34 @@
+<!---Created by  Pendam -->
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Car Rental Services</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <h1><i>CityWheels Rentals</i></h1>
+        <h2><b>Welcome to our Car Rental Services!</b></h2>
+        <!--included menuList.php file to display menu items-->
+        <?php include 'MenuList.php'; ?>
+    </header>
+    <main>
+        <h2>FIND YOUR BEST CAR RENTAL SERVICES WITH CITYWHEELS</h2>
+        <?php
+       echo " <p>
+        Welcome to CityWheels, Simplify your travel with convenience,<br>
+        comfort, and confidence. Whether it's a weekend <br>getaway or business trip, trust us as your transportation partner.
+        </p>"
+        ?>
+        <div class="background">
+        </div>
+    </main>
+
+
+
+
+</body>
+
+</html>
---
+++ MenuList.php
@@ -0,0 +1,21 @@
+<!-- created by pendam-->
+<link rel="stylesheet" href="style.css">
+<div class="navbar">
+    <div class="navlinks">
+        <ul>
+            <li><a href="Home.php">Home</a></li>
+            <li class="dropdown">
+                <a href="Reservation.php" class="dropbtn">Reservation</a>
+                <div class="dropdown-content">
+                    <p><a href="MakeReservation.php">Make A Reservation</a></p>
+                    <p><a href="ViewModifyCancelReservation.php">View/Modify/Cancel</a></p>
+                    <p><a href="GetEReceipt.php">Get E-Receipt</a></p>
+                </div>
+            </li>
+            <li><a href="Offers.php">Offers</a></li>
+            <li><a href="FeedBackAndReviews.php">Feedback & Reviews</a></li>
+            <li><a href="ContactUs.php">Contact Us</a></li>
+            <li><a href="aboutUs.php">About Us</a></li>
+        </ul>
+    </div>
+</div>
